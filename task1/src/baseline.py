efe29fc76c91a8061d683451eccd806a041071a7
Alon Emanuel <alon@alonemanuel.com>
2019-06-05 22:37:37 +0300

    We have a vectorizer for words

--- task1/src/baseline.py
+++ task1/src/baseline.py
@@ -1,10 +1,22 @@
+from sklearn.feature_extraction.text import  CountVectorizer
+
 class baseline:
 	def __init__(self, X_train, y_train):
-		self.X_train = X_train
+		self.X_train = self.preprocess(X_train)
 		self.y_train = y_train
 		self.train()
 
+	def preprocess(self, X):
+		'''
+		:param X:	shape=(n_samples, )
+		:return: 	shape=(n_samples, n_features)
+		'''
+		vectorizer = CountVectorizer()
+		X_vecd = vectorizer.fit_transform(X)
+		return X_vecd.toarray()
+
 	def train(self):
+
 		pass
 
 	def predict(self):
