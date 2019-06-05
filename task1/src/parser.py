819f187590af1077e1f930444129737ab44bd3a2
Alon Emanuel <alon@alonemanuel.com>
2019-06-05 21:13:24 +0300

    Started working on parser

--- task1/src/parser.py
+++ task1/src/parser.py
@@ -1,6 +1,14 @@
+from sklearn.model_selection import train_test_split
+
 class parser:
+	self.t
+
 	def __init__(self):
 		pass
 
 	def get_word_categories(self):
 		pass
+
+	def train_test_split(self):
+
+
