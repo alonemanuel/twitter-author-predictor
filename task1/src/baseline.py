f5da72e599a794caacbfbde1face0327c91c7024
Alon Emanuel <alon@alonemanuel.com>
2019-06-05 23:15:17 +0300

    Ready to test baseliner

--- task1/src/baseline.py
+++ task1/src/baseline.py
@@ -1,4 +1,7 @@
-from sklearn.feature_extraction.text import  CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from garcon import Garcon
+
+gc = Garcon()
 
 class Baseline:
 	def __init__(self, X_train, y_train):
@@ -6,19 +9,20 @@ class Baseline:
 		self.y_train = y_train
 		self.train()
 
-
 	def preprocess(self, X):
 		'''
 		:param X:	shape=(n_samples, )
 		:return: 	shape=(n_samples, n_features)
 		'''
+		gc.enter_func()
 		vectorizer = CountVectorizer()
 		X_vecd = vectorizer.fit_transform(X)
 		return X_vecd.toarray()
 
 	def train(self):
-
+		gc.enter_func()
 		pass
 
 	def predict(self):
+		gc.enter_func()
 		pass
--- task1/src/main.py
+++ task1/src/main.py
@@ -1,10 +1,18 @@
 from baseline import *
 from preprocessor import *
+from sklearn.metrics import confusion_matrix, classification_report
+
+def classifier_report(classifier, X_test, y_test):
+	y_hat = classifier.classify(X_test)
+	print(classification_report(y_test, y_hat))
+	print(confusion_matrix(y_test, y_hat))
+
 
 def main(X, y):
 	prep = Preprocessor()
 	X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
 	baseliner = Baseline(X_train, y_train)
-	
+	classifier_report(baseliner, X_test, y_test)
+
 if __name__ == '__main__':
 	main()
