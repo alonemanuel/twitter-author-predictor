df44ed30062a7e942fec6e08657960b35ae8ef14
Alon Emanuel <alon@alonemanuel.com>
2019-06-07 02:23:44 +0300

    Wrapping up

--- task1/src/learner_analyzer.py
+++ task1/src/learner_analyzer.py
@@ -1,30 +1,41 @@
-from sklearn.metrics import accuracy_score , confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 from task1.src.garcon import Garcon
 import numpy as np
 from sklearn.model_selection import train_test_split
 from task1.src.preprocess_data import DataPreProcessor
 
+
+
+
 gc = Garcon()
 TRAIN_RATIO = 0.85
-STEP_SIZE=5000
+STEP_SIZE = 5000
+
 
 class LearnerAnalyzer:
-	def __init__(self, learner, X_train, y_train):
-		self.learner = learner
-		self.X_train = X_train
-		self.y_train = y_train
+	'''
+	Class for analyzing and reporting learners.
+	'''
+	def __init__(self, X, y):
+		pass
+	def start_reporting(self):
+		self.plot_heatmap()
+
+	def plot_heatmap(self):
+		pass
+
 
 	def report(self):
 		self.report_bias_variance(type(self.learner).__name__)
 
 
-	def report_bias_variance(self,learner_name):
+	def report_bias_variance(self, learner_name):
 		gc.enter_func()
 		gc.init_plt()
 		max_train_size = self.X_train.shape[0]
 		# size_range = np.arange(5000, max_train_size, STEP_SIZE)
-		size_range = np.arange(max_train_size, max_train_size+1)
+		size_range = np.arange(max_train_size, max_train_size + 1)
 		train_accuracy = np.zeros(size_range.shape)
 		test_accuracy = np.zeros(size_range.shape)
 		for i, train_size in enumerate(size_range):
@@ -48,6 +59,7 @@ class LearnerAnalyzer:
 		plt.plot(size_range, test_accuracy, label='Test')
 		plt.legend()
 		gc.save_plt(learner_name)
+
 
 	def reportMat(self, X, y, prep):
 		y_pred = self.learner.predict(prep.processTweets(X))
