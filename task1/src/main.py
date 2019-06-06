ae809422388636403c0d2b5e1d4b982c83e10314
Xelanos <or3232@gmail.com>
2019-06-06 11:58:14 +0300

    Add files via upload

--- task1/src/main.py
+++ task1/src/main.py
@@ -1,8 +1,9 @@
-from baseline import *
-from preprocessor import *
-from poly_regression import PolyRegression
-
-gc  = Garcon()
+from src.baseline import*
+from src.preprocessor import *
+from src.poly_regression import PolyRegression
+from src.garcon import Garcon
+from src.preproccesData import TweetsPreProcessor
+gc = Garcon()
 
 
 def main():
@@ -14,9 +15,14 @@ def main():
 	# Helps in preprocessing tasks
 	prep = Preprocessor()
 	X, y = prep.get_train_data()
-	X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
-	baseliner = Baseline(X_train, y_train)
-	baseliner.report(X_test, y_test)
+	X_train, y_train, X_vault, y_vault = prep.train_test_split(X, y)
+	X_train, y_train, X_test, y_test = prep.train_test_split(X_train, y_train)
+
+	tweets_proccesor = TweetsPreProcessor()
+	proccesed_data = tweets_proccesor.processTweets(X_train)
+
+	# baseliner = Baseline(X_train, y_train)
+	# baseliner.report(X_test, y_test)
 	# X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
 	# # Different learning models
 	# models = [PolyRegression]
