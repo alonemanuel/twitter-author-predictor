dcfb90865e7e491c139f14287432e2708fa8cc2f
Alon Emanuel <alon@alonemanuel.com>
2019-06-06 09:07:58 +0300

    Why isn't this saving

--- task1/src/main.py
+++ task1/src/main.py
@@ -1,31 +1,29 @@
 from baseline import *
 from preprocessor import *
-from sklearn.metrics import confusion_matrix, classification_report
+from poly_regression import PolyRegression
 
-DATA_DIR = 'tweets_data'
 gc  = Garcon()
-
-def classifier_report(classifier, X_test, y_test):
-	'''
-	Creates reports for a given classifier.
-	:param classifier:		Classifier to report.
-	:param X_test: 			shape=(n_samples, )
-	:param y_test: 			shape=(n_samples, )
-	'''
-	gc.enter_func()
-	y_hat = classifier.classify(X_test)
-	print(classification_report(y_test, y_hat))
-	print(confusion_matrix(y_test, y_hat))
 
 
 def main():
+	'''
+	Runs everything, and in particular creates a class of hypotheses and
+	creates a report for each.
+	'''
 	gc.enter_func()
+	# Helps in preprocessing tasks
 	prep = Preprocessor()
 	X, y = prep.get_train_data()
-
 	X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
 	baseliner = Baseline(X_train, y_train)
 	baseliner.report(X_test, y_test)
+	# X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
+	# # Different learning models
+	# models = [PolyRegression]
+	# learners = [None]
+	# for i, model in enumerate(models):
+	# 	learners[i] = model(X_train, y_train)	# Training is done uon creation
+	# 	learners[i].report(X_test, y_test)
 
 
 if __name__ == '__main__':
