fa20da67768abae3b8ee24bf0e163b0f9f553d58
xelanos <or3232@gmail.com>
2019-06-07 02:07:39 +0300

    classififier ready for submit

--- task1/src/classifier.py
+++ task1/src/classifier.py
@@ -1,16 +1,11 @@
-class classifier:
-	def __init__(self, X_train, y_train):
-		self.X_train = X_train
-		self.y_train = y_train
+import pickle
 
-		self.train()
-
-	def train(self):
-		pass
-
-	def classify(self, X):
-		'''
-		:param X:	samples to tag. shape=(n_samples, )
-		:return:	y_hat = labels (0-9) of samples. shape=(n_samples, )
-		'''
-		pass
+def classify(tweets):
+	'''
+	:param tweets:	samples to tag. shape=(n_samples, )
+	:return:	y_hat = labels (0-9) of samples. shape=(n_samples, )
+	'''
+	classifier_f = open('classify-final-build.pickle', 'rb')
+	classifier = pickle.load(classifier_f)
+	classifier_f.close()
+	return classifier.predict(tweets)
