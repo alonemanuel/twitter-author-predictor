41a2e8ea0de96dc70767195dff0d0d1c29c639ca
daniellevin <daniil.levin@mail.huji.ac.il>
2019-06-06 23:00:30 +0300

    Works really fast ~0.80

--- task1/src/main.py
+++ task1/src/main.py
@@ -5,66 +5,67 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.metrics import classification_report, confusion_matrix
 import re
+from sklearn.ensemble import RandomForestClassifier
+
 from task1.src.preproccesData import TweetsPreProcessor
 from sklearn.linear_model import LogisticRegression
 
-
 from sklearn.feature_selection import SelectKBest, chi2
-# from task1.src import baseline
-from task1.src.learners.decision_tree import DecisionTree
-from task1.src.learners.random_forest import RandomForest
-from task1.src.learners.l_d_a import LDA
-from task1.src.learners.knn import KNN
-from task1.src.learners.learner_analyzer import LearnerAnalyzer
-from task1.src.learners.logistic_regression import LogisticReg
-from task1.src.dataGetter import *
 import numpy as np
-from sklearn.pipeline import Pipeline
 
+from task1.src.learners.learner_analyzer import LearnerAnalyzer
+from task1.src.dataGetter import DataGetter
+
+from sklearn.pipeline import Pipeline
 
 gc = Garcon()
 
 
 def main():
-	'''
+    '''
 	Runs everything, and in particular creates a class of hypotheses and
 	creates a report for each.
 	'''
-	gc.enter_func()
+    gc.enter_func()
 
-	prep = DataGetter()
-	X, y = prep.get_train_data()
-	X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
+    prep = DataGetter()
+    X, y = prep.get_train_data()
+    X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
 
-	# prep = TweetsPreProcessor(X_train)
-	# data = prep.processTweets(X_train, y_train)
+    # prep = TweetsPreProcessor(X_train)
+    # data = prep.processTweets(X_train, y_train)
 
-	vectorizer = TfidfVectorizer(min_df=3, stop_words="english", sublinear_tf=True, norm='l2', ngram_range=(1, 2))
+    vectorizer = TfidfVectorizer(min_df=3, stop_words="english", sublinear_tf=True, norm='l2', ngram_range=(1, 2))
 
-	pipeline = Pipeline([('vect', vectorizer),
-						 ('chi',  SelectKBest(chi2, k=9000)),
-						 ('clf', LogisticRegression())])
-	# fitting our model and save it in a pickle for later use
-	model = pipeline.fit(X_train, y_train)
-	ytest = np.array(y_test)
-	# confusion matrix and classification report(precision, recall, F1-score)
-	print(classification_report(ytest, model.predict(X_test)))
-	print(confusion_matrix(ytest, model.predict(X_test)))
+    # vectorizer
 
-	# KNN, Multinomial Naive Bayes, Linear SVC, and Random Forrest
+    pipeline = Pipeline([('vect', vectorizer),
+                         ('chi', SelectKBest(chi2, k=10000)),
+                         ('clf', RandomForestClassifier(max_depth=500, n_estimators=15))])
+                         # ('clf', LogisticRegression(solver='sag', penalty='l2', tol=1e-5))])
+    # fitting our model and save it in a pickle for later use
+    model = pipeline.fit(X_train, y_train)
+    ytest = np.array(y_test)
+    # confusion matrix and classification report(precision, recall, F1-score)
+    print(classification_report(y_train, model.predict(X_train)))
+    print(classification_report(ytest, model.predict(X_test)))
+    print(confusion_matrix(ytest, model.predict(X_test)))
 
-	# Helps in preprocessing tasks
-	# prep = Preprocessor()
-	# X, y = prep.get_train_data()
-	# X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
-	# # Different learning models
-	# models = [LogisticReg]#, RandomForest, DecisionTree, LDA]#KNN
-	# learners = [None, None, None]
-	# for i, model in enumerate(models):
-	# 	print("\n\nPerformance of " + str(model.__name__) + " model:\n\n")
-	# 	analyzer = LearnerAnalyzer(model, X_train, y_train)
-	# 	analyzer.report()
+
+# KNN, Multinomial Naive Bayes, Linear SVC, and Random Forrest
+
+# Helps in preprocessing tasks
+# prep = Preprocessor()
+# X, y = prep.get_train_data()
+# X_train, y_train, X_test, y_test = prep.train_test_split(X, y)
+# # Different learning models
+# models = [LogisticReg]#, RandomForest, DecisionTree, LDA]#KNN
+# learners = [None, None, None]
+# for i, model in enumerate(models):
+# 	print("\n\nPerformance of " + str(model.__name__) + " model:\n\n")
+# 	analyzer = LearnerAnalyzer(model, X_train, y_train)
+# 	analyzer.report()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- task1/src/preprocessor.py
+++ task1/src/preprocessor.py
@@ -18,104 +18,105 @@ TRAIN_RATIO = 0.85
 DATA_DIR_PATH = 'tweets_data'
 CSV_EXT = ".csv"
 
+
 class Preprocessor:
-	'''
+    '''
 	Preprocesses the data.
 	'''
 
-	def __init__(self, raw_X, raw_y):
-		'''
+    def __init__(self, raw_X, raw_y):
+        '''
 		Ctor for a preprocessor.
 		:param tweets:	type=df, shape=(n_samples, 2)
 		'''
 
-		self.tweets = raw_X
-		self.labels = raw_y
-		self.vectorizer = None
+        self.tweets = raw_X
+        self.labels = raw_y
+        self.vectorizer = None
 
-		self.common_hashtags = []
-		self.common_mentions = []
-		self.common_lexemes = []
+        self.common_hashtags = []
+        self.common_mentions = []
+        self.common_lexemes = []
 
-	def preprocess(self):
-		'''
+    def preprocess(self):
+        '''
 		Begins preprocessing.
 		'''
-		# self._split_tweets_labels()
-		self._set_vectorizer()
-		self._extract_indicators()
+        # self._split_tweets_labels()
+        self._set_vectorizer()
+        self._extract_indicators()
 
-	def _split_tweets_labels(self):
-		'''
+    def _split_tweets_labels(self):
+        '''
 		Splits raw data into tweets and labels.
 		'''
-		self.tweets = self.raw_data['tweet']
-		self.labels = self.raw_data['user']
+        self.tweets = self.raw_data['tweet']
+        self.labels = self.raw_data['user']
 
-	def _set_vectorizer(self):
-		'''
+    def _set_vectorizer(self):
+        '''
 		Inits a vectorizer for the bag of words.
 		'''
-		if IS_TFIDF:
-			self._tfidf_vectorize()
-		else:
-			self._count_vectorize()
-		self.vectorizer.fit(self.tweets)
+        if IS_TFIDF:
+            self._tfidf_vectorize()
+        else:
+            self._count_vectorize()
+        self.vectorizer.fit(self.tweets)
 
-	def _tfidf_vectorize(self):
-		self.vectorizer = TfidfVectorizer(smooth_idf=False, sublinear_tf=False,
-										  norm=None, analyzer='word')
+    def _tfidf_vectorize(self):
+        self.vectorizer = TfidfVectorizer(smooth_idf=False, sublinear_tf=False,
+                                          norm=None, analyzer='word')
 
-	def _count_vectorize(self, min_ngram=2, max_ngram=3,
-						 min_df=0.001, max_df=0.99, max_feat=10000,
-						 binary=False):
-		self.vectorizer = CountVectorizer(stop_words='english', analyzer='word',
-										  ngram_range=(min_ngram, max_ngram),
-										  min_df=min_df, max_df=max_df,
-										  max_features=max_feat, binary=binary)
+    def _count_vectorize(self, min_ngram=2, max_ngram=3,
+                         min_df=0.001, max_df=0.99, max_feat=10000,
+                         binary=False):
+        self.vectorizer = CountVectorizer(stop_words='english', analyzer='word',
+                                          ngram_range=(min_ngram, max_ngram),
+                                          min_df=min_df, max_df=max_df,
+                                          max_features=max_feat, binary=binary)
 
-	def _extract_indicators(self):
-		self._get_common_hashtags()
-		self._get_common_mentions()
-		self._get_common_lexemes()
+    def _extract_indicators(self):
+        self._get_common_hashtags()
+        self._get_common_mentions()
+        self._get_common_lexemes()
 
-	def _get_common_hashtags(self):
-		pass
+    def _get_common_hashtags(self):
+        pass
 
-	def _get_common_mentions(self):
-		pass
+    def _get_common_mentions(self):
+        pass
 
-	def _get_common_lexemes(self):
-		pass
+    def _get_common_lexemes(self):
+        pass
 
-	def get_tweets_features(self, new_tweets):
-		'''
+    def get_tweets_features(self, new_tweets):
+        '''
 		Extracts features from tweets.
 		:param new_tweets:	type=df, 		shape=(n_tweets, )
 		:return: 			type=np.array, 	shape=(n_tweets, n_features)
 		'''
-		bow_df = self._get_bow_df(new_tweets)
-		indicators_df = self._get_indicators_df(new_tweets)
-		features = pd.concat([bow_df, indicators_df], axis='columns')
-		return features.values
+        bow_df = self._get_bow_df(new_tweets)
+        indicators_df = self._get_indicators_df(new_tweets)
+        features = pd.concat([bow_df, indicators_df], axis='columns')
+        return features.values
 
-	def _get_bow_df(self, tweets):
-		'''
+    def _get_bow_df(self, tweets):
+        '''
 		:param tweets:	type=df,	shape=(n_tweets, )
 		:return: 		type=df,	shape=(n_tweets, n_bags)
 		'''
-		bow = self.vectorizer.transform(tweets)
-		vecd_df = pd.DataFrame(bow.todense(),
-							   columns=self.vectorizer.get_feature_names())
-		vecd_df.to_csv('checking.csv')
-		return vecd_df
+        bow = self.vectorizer.transform(tweets)
+        vecd_df = pd.DataFrame(bow.todense(),
+                               columns=self.vectorizer.get_feature_names())
+        vecd_df.to_csv('checking.csv')
+        return vecd_df
 
-	def _get_indicators_df(self, tweets):
-		'''
+    def _get_indicators_df(self, tweets):
+        '''
 		:param tweets:	type=df,	shape=(n_tweets, )
 		:return: 		type=df,	shape=(n_tweets, n_indicators)
 		'''
-		return pd.DataFrame()
+        return pd.DataFrame()
 
 ##########################################
 ##########################################
